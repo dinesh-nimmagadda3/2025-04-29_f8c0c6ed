cb7ee3415af23344eac82c996618b6f2c14871c3
Dinesh Nimmagadda <dinesh.nimmagadda@outlook.com>
2025-05-21 10:08:25 +0100

    formatted

--- Scripts/desktop.js
+++ Scripts/desktop.js
@@ -1,147 +1,145 @@
+window._currentDevice = "desktop";
+window.Parameters = window.Parameters || {
+  HomeUrl: "http://app.multiscreensite.com/site/f8c0c6ed",
+  AccountUUID: "8892e4ca1c284e93a7561cc931ccb513",
+  SystemID: "US_DIRECT_PRODUCTION",
+  SiteAlias: "f8c0c6ed",
+  SiteType: atob("RFVEQU9ORQ=="),
+  PublicationDate: "Wed Mar 12 20:58:51 UTC 2025",
+  ExternalUid: null,
+  IsSiteMultilingual: false,
+  InitialPostAlias: "",
+  InitialPostPageUuid: "",
+  InitialDynamicItem: "",
+  DynamicPageInfo: {
+    isDynamicPage: false,
+    base64JsonRowData: "null",
+  },
+  InitialPageAlias: "home",
+  InitialPageUuid: "f24a8bd2da0c4f70a8872124e724cb45",
+  InitialPageId: "35136936",
+  InitialEncodedPageAlias: "aG9tZQ==",
+  InitialHeaderUuid: "078f3d24818f419087ed03a2ba7a36ea",
+  CurrentPageUrl: "",
+  IsCurrentHomePage: true,
+  AllowAjax: false,
+  AfterAjaxCommand: null,
+  HomeLinkText: "Back To Home",
+  UseGalleryModule: false,
+  CurrentThemeName: "Layout Theme",
+  ThemeVersion: "52700",
+  DefaultPageAlias: "",
+  RemoveDID: true,
+  WidgetStyleID: null,
+  IsHeaderFixed: false,
+  IsHeaderSkinny: false,
+  IsBfs: true,
+  StorePageAlias: "null",
+  StorePagesUrls: "e30=",
+  IsNewStore: "false",
+  StorePath: "",
+  StoreId: "null",
+  StoreVersion: 0,
+  StoreBaseUrl:
+    "/site/f8c0c6ed?preview=true&dm_device=desktop&dm_exportSite=true&nossl&dm_exportSite_protected=9eaf1b90_1745921351741_5_7082468d5a1e9d6304619bf6bde99ce9c8a49037b7fe0cc2708a2c94d7dcd014",
+  StoreCleanUrl: true,
+  StoreDisableScrolling: true,
+  IsStoreSuspended: false,
+  HasCustomDomain: true,
+  SimpleSite: false,
+  showCookieNotification: false,
+  cookiesNotificationMarkup: "null",
+  translatedPageUrl: "",
+  isFastMigrationSite: false,
+  sidebarPosition: "NA",
+  currentLanguage: "en",
+  currentLocale: "en",
+  NavItems: "{}",
+  errors: {
+    general:
+      "There was an error connecting to the page.<br/> Make sure you are not offline.",
+    password: "Incorrect name/password combination",
+    tryAgain: "Try again",
+  },
+  NavigationAreaParams: {
+    ShowBackToHomeOnInnerPages: true,
+    NavbarSize: 4,
+    NavbarLiveHomePage: "http://app.multiscreensite.com/site/f8c0c6ed",
+    BlockContainerSelector: ".dmBody",
+    NavbarSelector: "#dmNav:has(a)",
+    SubNavbarSelector: "#subnav_main",
+  },
+  hasCustomCode: false,
+  planID: "7",
+  customTemplateId: "null",
+  siteTemplateId: "null",
+  productId: "DM_DIRECT",
+  disableTracking: false,
+  pageType: "FROM_SCRATCH",
+  isRuntimeServer: true,
+  isInEditor: false,
+  hasNativeStore: false,
+  defaultLang: "en",
+  hamburgerMigration: null,
+  isFlexSite: false,
+};
 
-    window._currentDevice = 'desktop';
-    window.Parameters = window.Parameters || {
-        HomeUrl: 'http://app.multiscreensite.com/site/f8c0c6ed',
-        AccountUUID: '8892e4ca1c284e93a7561cc931ccb513',
-        SystemID: 'US_DIRECT_PRODUCTION',
-        SiteAlias: 'f8c0c6ed',
-        SiteType: atob('RFVEQU9ORQ=='),
-        PublicationDate: 'Wed Mar 12 20:58:51 UTC 2025',
-        ExternalUid: null,
-        IsSiteMultilingual: false,
-        InitialPostAlias: '',
-        InitialPostPageUuid: '',
-        InitialDynamicItem: '',
-        DynamicPageInfo: {
-            isDynamicPage: false,
-            base64JsonRowData: 'null',
-        },
-        InitialPageAlias: 'home',
-        InitialPageUuid: 'f24a8bd2da0c4f70a8872124e724cb45',
-        InitialPageId: '35136936',
-        InitialEncodedPageAlias: 'aG9tZQ==',
-        InitialHeaderUuid: '078f3d24818f419087ed03a2ba7a36ea',
-        CurrentPageUrl: '',
-        IsCurrentHomePage: true,
-        AllowAjax: false,
-        AfterAjaxCommand: null,
-        HomeLinkText: 'Back To Home',
-        UseGalleryModule: false,
-        CurrentThemeName: 'Layout Theme',
-        ThemeVersion: '52700',
-        DefaultPageAlias: '',
-        RemoveDID: true,
-        WidgetStyleID: null,
-        IsHeaderFixed: false,
-        IsHeaderSkinny: false,
-        IsBfs: true,
-        StorePageAlias: 'null',
-        StorePagesUrls: 'e30=',
-        IsNewStore: 'false',
-        StorePath: '',
-        StoreId: 'null',
-        StoreVersion: 0,
-        StoreBaseUrl: '/site/f8c0c6ed?preview=true&dm_device=desktop&dm_exportSite=true&nossl&dm_exportSite_protected=9eaf1b90_1745921351741_5_7082468d5a1e9d6304619bf6bde99ce9c8a49037b7fe0cc2708a2c94d7dcd014',
-        StoreCleanUrl: true,
-        StoreDisableScrolling: true,
-        IsStoreSuspended: false,
-        HasCustomDomain: true,
-        SimpleSite: false,
-        showCookieNotification: false,
-        cookiesNotificationMarkup: 'null',
-        translatedPageUrl: '',
-        isFastMigrationSite: false,
-        sidebarPosition: 'NA',
-        currentLanguage: 'en',
-        currentLocale: 'en',
-        NavItems: '{}',
-        errors: {
-            general: 'There was an error connecting to the page.<br/> Make sure you are not offline.',
-            password: 'Incorrect name/password combination',
-            tryAgain: 'Try again'
-        },
-        NavigationAreaParams: {
-            ShowBackToHomeOnInnerPages: true,
-            NavbarSize: 4,
-            NavbarLiveHomePage: 'http://app.multiscreensite.com/site/f8c0c6ed',
-            BlockContainerSelector: '.dmBody',
-            NavbarSelector: '#dmNav:has(a)',
-            SubNavbarSelector: '#subnav_main'
-        },
-        hasCustomCode: false,
-        planID: '7',
-        customTemplateId: 'null',
-        siteTemplateId: 'null',
-        productId: 'DM_DIRECT',
-        disableTracking: false,
-        pageType: 'FROM_SCRATCH',
-        isRuntimeServer: true,
-        isInEditor: false,
-        hasNativeStore: false,
-        defaultLang: 'en',
-        hamburgerMigration: null,
-        isFlexSite : false
-    };
+window.Parameters.LayoutID = {};
+window.Parameters.LayoutID[window._currentDevice] = 6;
+window.Parameters.LayoutVariationID = {};
+window.Parameters.LayoutVariationID[window._currentDevice] = 5;
 
-    window.Parameters.LayoutID = {};
-    window.Parameters.LayoutID[window._currentDevice] = 6;
-    window.Parameters.LayoutVariationID = {};
-    window.Parameters.LayoutVariationID[window._currentDevice] = 5;
+window.SystemID = "US_DIRECT_PRODUCTION";
 
+if (!window.dmAPI) {
+  window.dmAPI = {
+    registerExternalRuntimeComponent: function () {},
+    getCurrentDeviceType: function () {
+      return window._currentDevice;
+    },
+    runOnReady: (ns, fn) => {
+      const safeFn = dmAPI.toSafeFn(fn);
+      ns = ns || "global_" + Math.random().toString(36).slice(2, 11);
+      const eventName = "afterAjax." + ns;
 
-    window.SystemID = 'US_DIRECT_PRODUCTION';
-
-    if (!window.dmAPI) {
-        window.dmAPI = {
-            registerExternalRuntimeComponent: function () {
-            },
-            getCurrentDeviceType: function () {
-                return window._currentDevice;
-            },
-            runOnReady: (ns, fn) => {
-                const safeFn = dmAPI.toSafeFn(fn);
-                ns = ns || 'global_' + Math.random().toString(36).slice(2, 11);
-                const eventName = 'afterAjax.' + ns;
-
-                if (document.readyState === 'complete') {
-                    $.DM.events.off(eventName).on(eventName, safeFn);
-                    setTimeout(function () {
-                        safeFn({
-                            isAjax: false,
-                        });
-                    }, 0);
-                } else {
-                    window?.waitForDeferred?.('dmAjax', () => {
-                        $.DM.events.off(eventName).on(eventName, safeFn);
-                        safeFn({
-                            isAjax: false,
-                        });
-                    });
-                }
-            },
-            toSafeFn: (fn) => {
-                if (fn?.safe) {
-                    return fn;
-                }
-                const safeFn = function (...args) {
-                    try {
-                        return fn?.apply(null, args);
-                    } catch (e) {
-                        console.log('function failed ' + e.message);
-                    }
-                };
-                safeFn.safe = true;
-                return safeFn;
-            }
-        };
-    }
-
-    if (!window.requestIdleCallback) {
-        window.requestIdleCallback = function (fn) {
-            setTimeout(fn, 0);
+      if (document.readyState === "complete") {
+        $.DM.events.off(eventName).on(eventName, safeFn);
+        setTimeout(function () {
+          safeFn({
+            isAjax: false,
+          });
+        }, 0);
+      } else {
+        window?.waitForDeferred?.("dmAjax", () => {
+          $.DM.events.off(eventName).on(eventName, safeFn);
+          safeFn({
+            isAjax: false,
+          });
+        });
+      }
+    },
+    toSafeFn: (fn) => {
+      if (fn?.safe) {
+        return fn;
+      }
+      const safeFn = function (...args) {
+        try {
+          return fn?.apply(null, args);
+        } catch (e) {
+          console.log("function failed " + e.message);
         }
-    }
+      };
+      safeFn.safe = true;
+      return safeFn;
+    },
+  };
+}
 
+if (!window.requestIdleCallback) {
+  window.requestIdleCallback = function (fn) {
+    setTimeout(fn, 0);
+  };
+}
 
 /**
  * There are a few <link> tags with CSS resource in them that are preloaded in the page
@@ -158,54 +156,59 @@
     link.rel = "stylesheet";
     link.type = "text/css";
   }
-  
-    function checkCss() {
-      const pageCssLink = document.querySelector("[id*='CssLink']");
-      const widgetCssLink = document.querySelector("[id*='widgetCSS']");
 
-        if (cssLinks && cssLinks.runtime && cssLinks.global && (!pageCssLink || cssLinks.page) && (!widgetCssLink || cssLinks.widget)) {
-            const storedRuntimeCssLink = cssLinks.runtime;
-            const storedPageCssLink = cssLinks.page;
-            const storedGlobalCssLink = cssLinks.global;
-            const storedWidgetCssLink = cssLinks.widget;
+  function checkCss() {
+    const pageCssLink = document.querySelector("[id*='CssLink']");
+    const widgetCssLink = document.querySelector("[id*='widgetCSS']");
 
-            storedGlobalCssLink.disabled = true;
-            loadCssLink(storedGlobalCssLink);
+    if (
+      cssLinks &&
+      cssLinks.runtime &&
+      cssLinks.global &&
+      (!pageCssLink || cssLinks.page) &&
+      (!widgetCssLink || cssLinks.widget)
+    ) {
+      const storedRuntimeCssLink = cssLinks.runtime;
+      const storedPageCssLink = cssLinks.page;
+      const storedGlobalCssLink = cssLinks.global;
+      const storedWidgetCssLink = cssLinks.widget;
 
-            if (storedPageCssLink) {
-                storedPageCssLink.disabled = true;
-                loadCssLink(storedPageCssLink);
-            }
+      storedGlobalCssLink.disabled = true;
+      loadCssLink(storedGlobalCssLink);
 
-            if(storedWidgetCssLink) {
-                storedWidgetCssLink.disabled = true;
-                loadCssLink(storedWidgetCssLink);
-            }
+      if (storedPageCssLink) {
+        storedPageCssLink.disabled = true;
+        loadCssLink(storedPageCssLink);
+      }
 
-            storedRuntimeCssLink.disabled = true;
-            loadCssLink(storedRuntimeCssLink);
+      if (storedWidgetCssLink) {
+        storedWidgetCssLink.disabled = true;
+        loadCssLink(storedWidgetCssLink);
+      }
 
-            requestAnimationFrame(() => {
-                setTimeout(() => {
-                    storedRuntimeCssLink.disabled = false;
-                    storedGlobalCssLink.disabled = false;
-                    if (storedPageCssLink) {
-                      storedPageCssLink.disabled = false;
-                    }
-                    if (storedWidgetCssLink) {
-                      storedWidgetCssLink.disabled = false;
-                    }
-                    // (SUP-4179) Clear the accumulated cssLinks only when we're
-                    // sure that the document has finished loading and the document 
-                    // has been parsed.
-                    if(document.readyState === 'interactive') {
-                      cssLinks = null;
-                    }
-                }, 0);
-            });
-        }
+      storedRuntimeCssLink.disabled = true;
+      loadCssLink(storedRuntimeCssLink);
+
+      requestAnimationFrame(() => {
+        setTimeout(() => {
+          storedRuntimeCssLink.disabled = false;
+          storedGlobalCssLink.disabled = false;
+          if (storedPageCssLink) {
+            storedPageCssLink.disabled = false;
+          }
+          if (storedWidgetCssLink) {
+            storedWidgetCssLink.disabled = false;
+          }
+          // (SUP-4179) Clear the accumulated cssLinks only when we're
+          // sure that the document has finished loading and the document
+          // has been parsed.
+          if (document.readyState === "interactive") {
+            cssLinks = null;
+          }
+        }, 0);
+      });
     }
-  
+  }
 
   function loadCSS(link) {
     try {
@@ -222,17 +225,13 @@
       } else if (link.id === "siteGlobalCss") {
         cssLinks.global = link;
         checkCss();
-      } 
-      
-      else if (link.id && link.id.includes("CssLink")) {
+      } else if (link.id && link.id.includes("CssLink")) {
         cssLinks.page = link;
         checkCss();
       } else if (link.id && link.id.includes("widgetCSS")) {
         cssLinks.widget = link;
         checkCss();
-      }
-      
-      else {
+      } else {
         requestIdleCallback(function () {
           window.setTimeout(function () {
             loadCssLink(link);
@@ -240,31 +239,32 @@
         });
       }
     } catch (e) {
-      throw e
+      throw e;
     }
   }
   window.loadCSS = window.loadCSS || loadCSS;
 })();
 
+/* usage: window.getDeferred(<deferred name>).resolve() or window.getDeferred(<deferred name>).promise.then(...)*/
+function Def() {
+  this.promise = new Promise(
+    function (a, b) {
+      (this.resolve = a), (this.reject = b);
+    }.bind(this)
+  );
+}
 
-    /* usage: window.getDeferred(<deferred name>).resolve() or window.getDeferred(<deferred name>).promise.then(...)*/
-    function Def() {
-        this.promise = new Promise((function (a, b) {
-            this.resolve = a, this.reject = b
-        }).bind(this))
-    }
-
-    const defs = {};
-    window.getDeferred = function (a) {
-        return null == defs[a] && (defs[a] = new Def), defs[a]
-    }
-    window.waitForDeferred = function (b, a, c) {
-        let d = window?.getDeferred?.(b);
-        d
-            ? d.promise.then(a)
-            : c && ["complete", "interactive"].includes(document.readyState)
-                ? setTimeout(a, 1)
-                : c
-                    ? document.addEventListener("DOMContentLoaded", a)
-                    : console.error(`Deferred  does not exist`);
-    };
+const defs = {};
+window.getDeferred = function (a) {
+  return null == defs[a] && (defs[a] = new Def()), defs[a];
+};
+window.waitForDeferred = function (b, a, c) {
+  let d = window?.getDeferred?.(b);
+  d
+    ? d.promise.then(a)
+    : c && ["complete", "interactive"].includes(document.readyState)
+    ? setTimeout(a, 1)
+    : c
+    ? document.addEventListener("DOMContentLoaded", a)
+    : console.error(`Deferred  does not exist`);
+};
